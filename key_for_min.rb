32fef4a18728088d21a4a65a332299c8f843d5ff
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-13 23:19:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   smallest_value = 1000000
   name_hash.each do |key, value|
     if name_hash[key][value] < smallest_value
-
+      smallest_value = name_hash[key][value]
     end
   end
 end
